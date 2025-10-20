71b72a095746ae70241c578a031fde4a217c8616
jazmin-H <huaynocajazmin999@gmail.com>
2025-10-20 00:32:37 -0300

    falta ajustar el test de cerrar sesión

---
+++ derecho-inte/tests/cerrarSesion.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/auth/login');
+  await page.getByRole('textbox', { name: 'correo electrónico' }).click();
+  await page.getByRole('textbox', { name: 'correo electrónico' }).fill('web.admin@gmail.com');
+  await page.getByRole('textbox', { name: 'contraseña' }).click();
+  await page.getByRole('textbox', { name: 'contraseña' }).fill('webadmin');
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await expect(page.getByRole('main')).toContainText('Bienvenido web.admin_test admin');
+  await page.getByRole('textbox', { name: 'correo electrónico' }).click();
+  await page.getByRole('textbox', { name: 'correo electrónico' }).fill('web.admin@gmail.com');
+  await page.getByRole('textbox', { name: 'contraseña' }).click();
+  await page.getByRole('textbox', { name: 'contraseña' }).fill('webadmin');
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.goto('http://localhost:3000/dashboard');
+  await page.getByRole('textbox', { name: 'correo electrónico' }).click();
+  await page.getByRole('textbox', { name: 'correo electrónico' }).press('ControlOrMeta+c');
+  await page.getByRole('textbox', { name: 'correo electrónico' }).fill('web.admin@gmail.com');
+  await page.getByRole('textbox', { name: 'contraseña' }).click();
+  await page.getByRole('textbox', { name: 'contraseña' }).fill('webadmin');
+  await page.getByRole('button', { name: 'Ingresar' }).click();
+  await page.getByRole('button', { name: 'Cerrar sesión' }).click();
+  await page.goto('http://localhost:3000/auth/login');
+});
